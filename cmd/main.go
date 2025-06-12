ef3b3d2dbbc14fd3be2f1c6e972caaae78495b47
peerapon.ph <peerapon.ph@kkumail.com>
2025-06-13 05:38:36 +0700

    Refactor🔥: Enhance main.go by organizing webhook setup and improving readability

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"telegram-expense-bot/bot"
 	"telegram-expense-bot/controller"
@@ -13,9 +14,8 @@ import (
 
 func main() {
 
-
+	// Load environment variables from .env.deploy file
 	_ = godotenv.Load(".env.deploy")
-
 
 	// Prepair Google Sheet Service
 	sheetService, err := service.NewGoogleSheetService()
@@ -32,19 +32,34 @@ func main() {
 		log.Fatal("❌  TELEGRAM_BOT_TOKEN Not Found in .env")
 	}
 
+	// Get webhook URL from environment variable
+	webhookURL := os.Getenv("WEBHOOK_URL")
+
+	// Initialize Telegram Bot API
 	botAPI, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
 		log.Fatal("❌ Does Not Initialize Telegram Bot", err)
 	}
+
+
+	// Set webhook if WEBHOOK_URL is provided
+	webhookConfig, err := tgbotapi.NewWebhook(webhookURL)
+	if err != nil {
+		log.Fatal("❌ Failed to create webhook config:", err)
+	}
+	_, err = botAPI.Request(webhookConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set up HTTP server to listen for webhook requests
+	updates := botAPI.ListenForWebhook("/telegram-webhook")
+	go http.ListenAndServe(":8080", nil) // Must Listen on port 8080
+
 	log.Printf("✅ Initialize Telegram Bot @%s", botAPI.Self.UserName)
 
 	// Create BotHandler
 	handler := bot.NewBotHandler(botAPI, controller, sheetService)
-
-	// Run update
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-	updates := botAPI.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message != nil {
